80954cd74673233ca2cd76cc1abe8c60cd59cbe9
muhammad waleed khan <muhammadwaleedkhan@muhammads-MacBook-Pro.local>
2024-01-21 23:10:35 +0500

    small condition added on post show page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,7 +13,7 @@
                 <span class="text-gray-500 text-sm">| {{ $post->getReadingTime() }} min read</span>
             </div>
             <div class="flex items-center">
-                <span class="text-gray-500 mr-2">{{ $post->published_at->diffForHumans() }}</span>
+                <span class="text-gray-500 mr-2">{{ $post->published_at?->diffForHumans() }}</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.3"
                     stroke="currentColor" class="w-5 h-5 text-gray-500">
                     <path stroke-linecap="round" stroke-linejoin="round"
