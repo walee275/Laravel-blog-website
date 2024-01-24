7b928b97504052d481fc93fdc593cbd657e37ca9
muhammad waleed khan <muhammadwaleedkhan@muhammads-MacBook-Pro.local>
2024-01-24 08:43:41 +0500

    sample link removed from side bar

--- resources/views/partials/left_sidebar.blade.php
+++ resources/views/partials/left_sidebar.blade.php
@@ -4,7 +4,7 @@
     <nav class="text-base lg:text-sm w-60 pr-5">
         <h2 class="text-center font-bold my-2">Important Links</h2>
         <ul role="list" class="mb-9 space-y-2.5">
-            <x-left-side-bar-link text="Side Nav Link" :icon="''" :url="'#'"/>
+            {{-- <x-left-side-bar-link text="Side Nav Link" :icon="''" :url="'#'"/> --}}
             {{-- <x-left-side-bar-link text="Side Nav Link" :icon="''" />
             <x-left-side-bar-link text="Side Nav Link" :icon="''" />
             <x-left-side-bar-link text="Side Nav Link" :icon="''" /> --}}
