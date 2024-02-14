a3712f09d8c3d14ea3dd4f712d0a92aae4e81dd1
waleed khan <wk5356743@gmail.com>
2024-02-14 12:00:41 +0500

    categories badges changed from async to synchronous

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -1,4 +1,4 @@
 @props(['text_color', 'bg_color'])
-<button {{ $attributes }} class="rounded-xl px-3 py-1 text-base" style="color: {{ $text_color }}; background:{{ $bg_color }};">
+<a {{ $attributes }} class="rounded-xl px-3 py-1 text-base" style="color: {{ $text_color }}; background:{{ $bg_color }};">
     {{ $slot }}
-</button>
+</a>
